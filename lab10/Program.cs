3d72a79e48faa59b7aa07a29d7799087c668f35b
Iliya <iiliyxaii@gmail.com>
2020-03-24 08:32:13 +0300

    l1

---
+++ lab10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class Program
+    {
+        public class ustroistvo
+        {
+            public string proizvod { get; set; }
+            public string model { get; set; }
+            public int god_nachala_proizvod { get; set; }
+            public string standart_svazy { get; set; }
+        }
+
+        class mobilnyi : ustroistvo
+        {
+            public void naim(string proizvod, string model)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
